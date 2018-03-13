28426eb9401a94cc981484311364035c7237cf08
Daniel Gango <gango.daniel@sztaki.mta.hu>
2018-03-13 14:11:59 +0000

    contact lines added to drawing function

--- membrane/drawsensor.m
+++ membrane/drawsensor.m
@@ -1,5 +1,5 @@
 function drawsensor(x)
-global D L n d
+global D L n d contacts h
 % relative angles
 phi = x(1:3:3*n+1);
 % global orientations
@@ -17,6 +17,26 @@ Y = zeros(1,n+1);
 for i = 1:n
     Y(i+1)= sum(L.*cos(psi(1:i)));
 end
+% coordinates for drawing the contact thingies
+s.x = [0 0];
+s.y = [0 0];
+contactlines = repmat(s, [1 n]);
+for i = 1:n
+    % coordinates of the segment endpoints
+    XA = X(i);
+    XB = X(i+1);
+    YA = -Y(i);
+    YB = -Y(i+1);
+    % starting point of contact line
+    x1 = (XA+XB)/2;
+    y1 = (YA+YB)/2;
+    % endpoint of contact line
+    x2 = x1 + h*cos(psi(i)-pi);
+    y2 = y1 + h*sin(psi(i)-pi);
+    % save coordinates
+    contactlines(i).x = [x1 x2];
+    contactlines(i).y = [y1 y2];
+end
 
 figure
 line([0 D], [0 0], 'Color', 'k') % sensor base
@@ -24,7 +44,15 @@ hold on
 if d>0
 line([-10 D+10], [-D/2+d -D/2+d], 'Color', 'k') % contact surface
 end
-plot(X,-Y)
+plot(X,-Y) % sensor
+for i = 1:n
+    if ismember(i,contacts)
+        contactcolor = 'red';
+    else
+        contactcolor = 'blue';
+    end
+    line(contactlines(i).x, contactlines(i).y, 'Color', contactcolor);
+end
 xlim([-10, D+10]);
 ylim([-D/2-5, 5]);
 axis equal
