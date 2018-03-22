a3b51e4ddf94494d7e3cd9ee1e4e904a6abb97ef
Daniel Gango <gango.daniel@sztaki.mta.hu>
2018-03-22 18:03:19 +0000

    equations with approximate contact forces added

--- membrane/test.m
+++ membrane/test.m
@@ -1,10 +1,9 @@
 clear all
 
 global n D d L kt h phi_r contacts
-n = 20;
-contacts = [8 13]; % list of segments with assumed contact
+n = 50;
 D = 40; % diameter
-d = 5; % contact depth
+d = 6; % contact depth
 L = D*sin(pi/(2*n)); % segment length
 kt = 100; % spring stiffness
 h = 0.3; % lenght of contact segment
@@ -15,7 +14,8 @@ phi_r(1) = ang/2;
 phi_r(end) = ang/2;
 
 % initial values
-x0 = zeros(1,(n+1)*3 + length(contacts));
+%x0 = zeros(1,(n+1)*3 + length(contacts));
+x0 = zeros(1,(n+1)*3);
 % %-----------------------------------------
 % use relaxed state as initial condition
 for i = 1:n+1
@@ -24,7 +24,7 @@ end
 % %----------------------------------------
 options = optimoptions('fsolve','MaxFunctionEvaluations',80000,...
                         'MaxIterations', 5000);
-[x, fval, exitflag] = fsolve(@equations_push, x0, options);
+[x, fval, exitflag] = fsolve(@equations_approx, x0, options);
 
 drawsensor(x);
 
