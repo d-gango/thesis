263b75f5e073e8453057592274f0a8963f35d744
Daniel Gango <dg17093@bristol.ac.uk>
2018-05-18 16:04:33 +0100

    projection -- first try

---
+++ sensor_model/projection.m
@@ -0,0 +1,33 @@
+figure
+plot(Expt.pinPositions(:,1),Expt.pinPositions(:,2), '.')
+axis equal
+xlabel('x'); ylabel('y')
+title('original camera image')
+
+% figure
+% scatter3(pins.original_coordinates(1,:), -pins.original_coordinates(3,:),...
+%     pins.original_coordinates(2,:));
+% axis equal
+% xlabel('x'); ylabel('-z'); zlabel('y')
+
+f = 150;
+camera_position = 50;
+camera_coordinates = pins.original_coordinates;
+camera_coordinates(2,:) =camera_coordinates(2,:)-camera_position;
+
+% figure
+% scatter3(camera_coordinates(1,:), -camera_coordinates(3,:),...
+%     camera_coordinates(2,:));
+% axis equal
+% xlabel('x'); ylabel('-z'); zlabel('y')
+
+projected_coordinates =zeros(3,length(pins.alpha));
+for i = 1:length(pins.alpha)
+    projected_coordinates(:,i) = ...
+        f/camera_coordinates(2,i) * camera_coordinates(:,i);
+end
+figure
+plot(camera_coordinates(1,:),camera_coordinates(3,:), '.')
+axis equal
+xlabel('x'); ylabel('z');
+title('perspective projection')
